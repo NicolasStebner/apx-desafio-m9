6f4cc317aa98a72da4ba494e1cf7748ba19ac875
Nicolas Stebner <nicolas_stebner@hotmail.com>
2024-01-26 14:54:13 -0300

    index-modified

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,7 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { findOrCreateAuthWithCode, sendCode } from '../../controllers/authController';
 
 export default async function (req: NextApiRequest, res: NextApiResponse){
-  // const auth = await findOrCreateAuthWithCode(req.body.email)
-  const auth = await sendCode(req.body.email)
-  res.send(auth)
+  res.send({message: "api.index found"})
 }
